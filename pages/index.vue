3ad153a38a2acae14db5e42cb5e1d1f2480b9d31
Mathew Morse <mrm1779@gmail.com>
2020-12-01 18:42:23 -0500

    Swap google analytics for hotjar

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
   <v-app class="">
-    <!-- Google Tag Manager -->
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-KFRWP7V');</script>
-    <!-- End Google Tag Manager -->
+    <!-- Hotjar Tracking Code for homemadecrunch.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2132984,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 
     <!-- Facebook Pixel Code -->
       <script>
@@ -27,11 +31,6 @@
     <!-- End Facebook Pixel Code -->
     <Navigation/>
       <v-main>
-        <!-- Google Tag Manager (noscript) -->
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KFRWP7V"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
-
         <v-banner
           class="state-alert"
           transition="slide-y-transition"
