34dd5941c53a252555e411115ad082345c07087a
Mathew Morse <mrm1779@gmail.com>
2020-12-02 17:42:35 -0500

    Giving splitbee a chance

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,6 @@
 <template>
-  <v-app class="">
-    <!-- Hotjar Tracking Code for homemadecrunch.com -->
-    <script>
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:2132984,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
+  <v-app>
+    <script async src="https://cdn.splitbee.io/sb.js"></script>
 
     <!-- Facebook Pixel Code -->
       <script>
