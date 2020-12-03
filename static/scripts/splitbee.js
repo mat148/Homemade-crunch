4cff4e94fef1735a010e17b63ec27c0bbb0338bd
Mathew Morse <mrm1779@gmail.com>
2020-12-02 19:52:17 -0500

    Source click/hover events added

--- static/scripts/splitbee.js
+++ static/scripts/splitbee.js
@@ -7,8 +7,25 @@ document.addEventListener('DOMContentLoaded', function(event) {
   source_honey_link = document.getElementsByClassName('source-honey-link');
   source_maple_link = document.getElementsByClassName('source-maple-link');
 
+  //SOURCE HOVER EVENTS
+  source_oats_link[0].addEventListener('mouseover', function(event) {
+    window.splitbee.track("Source_oats_link_hover");
+  });
+  source_honey_link[0].addEventListener('mouseover', function(event) {
+    window.splitbee.track("Source_honey_link_hover");
+  });
+  source_maple_link[0].addEventListener('mouseover', function(event) {
+    window.splitbee.track("Source_maple_link_hover");
+  });
+
   //SOURCE CLICK EVENTS
   source_oats_link[0].addEventListener('click', function(event) {
-    window.splitbee.track("Test event");
+    window.splitbee.track("Source_oats_link_click");
+  });
+  source_honey_link[0].addEventListener('click', function(event) {
+    window.splitbee.track("Source_honey_link_click");
+  });
+  source_maple_link[0].addEventListener('click', function(event) {
+    window.splitbee.track("Source_maple_link_click");
   });
 });
